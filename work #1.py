a0184a331734fb79387049654bc2cc7388577b2d
Aleksandr <Brodskiyvladimir@mail.ru>
2024-09-15 21:55:02 +0300

    Add files via upload

---
+++ work #1.py
@@ -0,0 +1,13 @@
+print (9 ** 0.5 * 5)            #1st program
+
+print (9.99 > 9.98 and 1000 != 1000.1)        #2nd program
+
+print (2 * 2 + 2)                    #3nd program
+print (2 * (2 + 2))
+print (2 * 2 + 2 == 2 * (2 + 2))
+
+print (float('123.456'))   #4nd program
+print (123.456 * 10)
+print (int(1234.56 / 10))
+print (int(123 / 10))
+print (int(12 / 10))
